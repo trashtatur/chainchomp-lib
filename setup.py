586a3e7fb1f3352ad6a0b250bec009ba41ee6f43
Andreas Hörster <andreas-hoerster@web.de>
2020-08-08 14:41:08 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chainchomp_lib',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='Library for commons for chainchomp',
     url='https://github.com/trashtatur/chainchomp_lib/',
