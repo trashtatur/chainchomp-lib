01b1a8ed331ea130b1448491d0bd69148d5b68f1
trashtatur <andreas-hoerster@web.de>
2020-11-01 15:54:20 +0100

    Correct env based folder function

--- chainchomplib/data/PathProvider.py
+++ chainchomplib/data/PathProvider.py
@@ -6,14 +6,14 @@ def env_based_base_folder():
     if 'CHAINCHOMP_TEST' not in os.environ:
         return Path.home()
 
-    if os.environ.get('CHAINCHOMP_TEST') is 0:
+    if os.environ.get('CHAINCHOMP_TEST') is '0':
         return Path.home()
 
-    if os.environ.get('CHAINCHOMP_TEST') is 1:
+    if os.environ.get('CHAINCHOMP_TEST') is '1':
         if 'CHAINCHOMP_TEST_DIR' not in os.environ:
             return Path.home()
 
-        return os.environ.get('CHAINCHOMP_TEST_DIR')
+        return Path(os.environ.get('CHAINCHOMP_TEST_DIR'))
 
 
 def base_config_folder():
