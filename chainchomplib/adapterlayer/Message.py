2cfafc95c645156293c3c127b41edb01be61b16a
trashtatur <andreas-hoerster@web.de>
2020-11-29 14:45:19 +0100

    MAke message serializable

--- chainchomplib/adapterlayer/Message.py
+++ chainchomplib/adapterlayer/Message.py
@@ -6,3 +6,14 @@ class Message:
     def __init__(self, message_body, message_header: MessageHeader):
         self._message_body = message_body
         self.message_header = message_header
+
+    def get_serialized(self):
+        return {
+            'message_body': self._message_body,
+            'message_header': {
+                'origin': self.message_header.origin,
+                'recipients': self.message_header.recipients,
+                'adapter_name': self.message_header.adapter_name,
+            }
+        }
+
