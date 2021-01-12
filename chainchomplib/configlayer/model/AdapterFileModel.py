5f1726bf90831b84c48f6b7624752574c77a3fc7
trashtatur <andreas-hoerster@web.de>
2021-01-12 17:21:04 +0100

    ADd adapter file model

---
+++ chainchomplib/configlayer/model/AdapterFileModel.py
@@ -0,0 +1,26 @@
+class AdapterFileModel:
+    def __init__(
+            self,
+            name: str,
+            path: str,
+            start: str,
+            stop: str
+    ):
+        self.name = name
+        self.path = path
+        self.start = start
+        self.stop = stop
+
+    def __eq__(self, other):
+        return self.name == other.name \
+               and self.path == other.path \
+               and self.start == other.start \
+               and self.stop == other.stop
+
+    def get_serialized(self) -> dict:
+        return {
+            'name': self.name,
+            'path': self.path,
+            'start': self.start,
+            'stop': self.stop
+        }
