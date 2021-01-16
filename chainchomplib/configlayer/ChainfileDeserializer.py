eb7f2530968f0a2b39bad12f789a644ef668c0a8
trashtatur <andreas-hoerster@web.de>
2021-01-16 11:06:12 +0100

    Make deserializer safe again

--- chainchomplib/configlayer/ChainfileDeserializer.py
+++ chainchomplib/configlayer/ChainfileDeserializer.py
@@ -1,3 +1,5 @@
+from chainchomplib.configlayer.resolver.FunctionResolver import FunctionResolver
+
 from chainchomplib.exceptions.Exceptions import NotValidException
 
 from chainchomplib.configlayer.model.ChainfileModel import ChainfileModel
@@ -16,13 +18,34 @@ class ChainfileDeserializer:
         except NotValidException:
             return None
         else:
-            return ChainfileModel(
-                data['project'],
-                data['chainlink']['name'],
-                data['chainlink']['next'],
-                data['chainlink']['previous'],
-                data['start'],
-                data['stop'],
-                data['adapter'],
-                data['profile']
+            chainlink_sub_dict: dict = data.get('chainlink')
+            chainlink_name = chainlink_sub_dict.get('name')
+            chainlink_next = chainlink_sub_dict.get('next', None)
+            chainlink_previous = chainlink_sub_dict.get('previous', None)
+            start = data.get('start', None)
+            stop = data.get('stop', None)
+            adapter_type = data.get('adapter', None)
+            profile = data.get('profile', None)
+
+            model = ChainfileModel(
+                FunctionResolver.parse(data['project']),
+                FunctionResolver.parse(chainlink_name)
             )
+
+            if chainlink_next is not None:
+                model.next_links = [FunctionResolver.parse(link) for link in chainlink_next]
+
+            if chainlink_previous is not None:
+                model.previous_links = [FunctionResolver.parse(link) for link in chainlink_next]
+
+            if start is not None:
+                model.start = FunctionResolver.parse(start)
+
+            if stop is not None:
+                model.stop = FunctionResolver.parse(stop)
+
+            if adapter_type is not None:
+                model.adapter = FunctionResolver.parse(adapter_type)
+
+            if profile is not None:
+                model.profile = FunctionResolver.parse(profile)
