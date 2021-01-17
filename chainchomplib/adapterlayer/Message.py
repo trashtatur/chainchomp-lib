59681a1e67d8ef341fd01a74367cac3145c05e61
trashtatur <andreas-hoerster@web.de>
2021-01-17 13:42:01 +0100

    Move ChainlinkResolver to library

--- chainchomplib/adapterlayer/Message.py
+++ chainchomplib/adapterlayer/Message.py
@@ -4,12 +4,12 @@ from chainchomplib.adapterlayer.MessageHeader import MessageHeader
 class Message:
 
     def __init__(self, message_body, message_header: MessageHeader):
-        self._message_body = message_body
+        self.message_body = message_body
         self.message_header = message_header
 
     def get_serialized(self) -> dict:
         return {
-            'message_body': self._message_body,
+            'message_body': self.message_body,
             'message_header': {
                 'origin': self.message_header.origin,
                 'recipients': self.message_header.recipients,
